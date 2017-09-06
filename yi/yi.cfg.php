10ac889a77f8bb5b2f5ce6c997d6cfb79f26b279
windfoxofo <windfox_foundation@163.com>
2017-09-06 11:50:33 +0800

    Update yi.cfg.php

--- yi/yi.cfg.php
+++ yi/yi.cfg.php
@@ -16,3 +16,11 @@
 define('DEFAULT_FILE', 'index.yi,default.yi,index.html,default.html,index.htm,default,htm'); //默认文件顺序，用半角逗号分隔
 define('YI_DEBUG', true); //调试模式
 define('YI_USE_LIB', 'krnln');
+
+// 错误页设置 (相对于public_html目录，只支持html)
+$errpage['400'] = 'errpages/400.html';
+$errpage['403'] = 'errpages/403.html';
+$errpage['404'] = 'errpages/404.html';
+$errpage['405'] = 'errpages/405.html';
+$errpage['500'] = 'errpages/500.html';
+$errpage['others'] = 'errpages/others.html';
