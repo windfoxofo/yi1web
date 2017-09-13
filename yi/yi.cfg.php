3177836d5f49451a99dfd45b9d38ebced0ded654
windfoxofo <windfox_foundation@163.com>
2017-09-13 11:46:30 +0800

    Update yi.cfg.php

--- yi/yi.cfg.php
+++ yi/yi.cfg.php
@@ -13,7 +13,7 @@
 // [ 用户配置文件 ]
 
 // 公共设置
-define('DEFAULT_FILE', 'index.yi,default.yi,index.html,default.html,index.htm,default,htm'); //默认文件顺序，用半角逗号分隔
+define('DEFAULT_FILE', 'index.yi,default.yi,index.html,default.html,index.htm,default.htm'); //默认文件顺序，用半角逗号分隔
 define('YI_DEBUG', true); //调试模式
 define('YI_USE_LIB', 'krnln');
 
