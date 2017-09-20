766381cee03befd136f654d914f0e03e99cfbd56
windfoxofo <windfox_foundation@163.com>
2017-09-20 11:49:49 +0800

    Update yi_interpreter.php

--- yi/yi_interpreter.php
+++ yi/yi_interpreter.php
@@ -12,3 +12,11 @@
 
 // [ 翼语言解析入口文件 ]
 
+// 读取翼语言支持库
+$yi_libs = explode(',',YI_USE_LIB);
+// 检查支持库
+foreach ($yi_libs as $yilib){
+  if(!file_exists('./lib/'.$yilib.'/'.$yilib.'.lib.php')){
+    error_msg('找不到支持库 '.$yilib,'支持库 '.$yilib.' 不存在，
+  }
+}
